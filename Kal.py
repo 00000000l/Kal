64e4d2e73d6173cccad3efed11e4457cd1b4878f
00000000l <lil.danial99@gmail.com>
2024-05-16 22:45:31 +0330

    Update Kal.py

--- Kal.py
+++ Kal.py
@@ -1,10 +1,4 @@
 def igbombingwin():
-    from selenium import webdriver
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    from selenium.webdriver.chrome.options import Options
     from simple_chalk import chalk
     import platform
     import time
@@ -196,4 +190,4 @@ print(chalk.green.bold(Options))
 opt = int(input(chalk.blue.bold("    |-> Enter the option number : ")))
 
 if opt == 4:
-    igbombingwin()
+igbombingwin()
